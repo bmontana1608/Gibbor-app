104f73e2b64cee2581ffcc672b3ecda14504203f
Alex Toscano <escuelagibborfc@gmail.com>
2026-06-07 21:33:23 -0500

    feat: enable dark mode with next-themes and tailwindcss v4

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -5,9 +5,9 @@ import { Toaster } from 'sonner';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
       <Toaster richColors position="top-right" />
-    </>
+    </ThemeProvider>
   );
 }
